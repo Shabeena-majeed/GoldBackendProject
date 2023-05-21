a9ad4f82ace77cb6dd9ff0c59a39103ed5d2c656
Shabeena-majeed <shabeenamajeed00@gmail.com>
2023-05-21 13:50:31 -0700

    backend

---
+++ Models/CompanyDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GoldLiveCoreApp.Models
+{
+    public class CompanyContext
+        : DbContext
+    {
+        public CompanyContext(DbContextOptions options)
+            : base(options)
+        {
+
+        }
+
+        //public DbSet<Employee> Employees { get; set; }
+    }
+}
